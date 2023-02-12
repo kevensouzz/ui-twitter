16daa0dcab007f707470113b3b7f5abfc48bc2eb
Keven <kevenssouza1@gmail.com>
2023-02-12 13:15:35 -0300

    Update newTweet

--- src/Pages/Timeline/Timeline.tsx
+++ src/Pages/Timeline/Timeline.tsx
@@ -1,21 +1,30 @@
+import { FormEvent, useState } from 'react'
 import Header from '../../components/Header/Header'
 import Separator from '../../components/Separator/Separator'
 import Tweet from '../../components/Tweet/Tweet'
 import './styles/Timeline.css'
 
-const tweets = [
-    'Olá mundo!'  
-  ]
-
 const TimeLine = () => {
+  const [newTweet, setNewTweet] = useState('')
+
+  const [tweets, setTweets] = useState([
+    'Olá mundo!'  
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+    setTweets([newTweet, ...tweets])
+    setNewTweet('')
+  }
+
   return (
     <main className='timeLine'>
           <Header title='Home' />
 
-          <form className='new-tweet-form'>
+          <form onSubmit={createNewTweet} className='new-tweet-form'>
             <label htmlFor="tweet">
               <img src="https://github.com/kevensouzz.png" alt="Keven Souza" />
-              <textarea id='tweet' placeholder="what's happening?" />
+              <textarea id='tweet' placeholder="what's happening?" value={newTweet} onChange={(event) => setNewTweet(event.target.value)} />
             </label>
 
             <button type="submit">Tweet</button>
